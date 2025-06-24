4b408ad24c3b4291a9c2f05ed9ba982af260b666
zaineb-bouallegui <bouallagui.zaineb@esprit.tn>
2025-06-24 14:42:30 +0100

    Initial commit

---
+++ src/main/java/com/example/securetask/SecuretaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.securetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuretaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuretaskApplication.class, args);
+	}
+
+}
